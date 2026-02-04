5914c1debd08afd54a8c711b474e70d9cb5d70d5
R D <rusirud49@gmail.com>
2026-02-05 00:20:40 +0530

    create files

---
+++ formattingString/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt" 
+
+func main() {
+	age :=30
+	name := "John"
+	
+	// print
+	fmt.Print("hello ")
+	fmt.Print("wolrd\n")
+	fmt.Print("This is new line")
+
+	// println
+	fmt.Println("\nHi there")
+	fmt.Println("my name is", name, "and I am", age, "years old")
+}
+
